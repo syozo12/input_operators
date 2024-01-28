7e08d670ea2a61d5889f8655430341d07c695883
syozo12 <156905994+syozo12@users.noreply.github.com>
2024-01-28 14:01:33 +0530

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r,h;
+    float v;
+    cout<<"enter r and h";
+    cin>>r>>h;
+     v=3.14*r*r*h;
+    cout<<"volume is"<<v;
+    
+}
---
+++ 5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch1;
+    cout<<"enter first character";
+    cin>>ch1;
+    char ch2;
+    cout<<"enter 2 character";
+    cin>>ch2;
+    int x=int(ch1);
+    int y=int(ch2);
+    cout<<int(ch1)-int(ch2);
+    cout<<ch1-ch2;
+    cout<<x-y;
+}
